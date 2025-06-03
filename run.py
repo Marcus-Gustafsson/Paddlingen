4af9dd4e0747b61e7c8b6e172aefe7f1bb4c0587
Marcus-Gustafsson <marcus.thomas.gustafsson@hotmail.com>
2025-06-03 20:12:33 +0200

    css fixes for sidebar highlighting

--- run.py
+++ run.py
@@ -20,5 +20,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)   # Starts Flask on http://127.0.0.1:5000
+    app.run()   # Starts Flask on http://127.0.0.1:5000
 
